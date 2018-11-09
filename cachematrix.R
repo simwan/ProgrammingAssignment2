3c63b7184d09ff9f628952e6e130a30e7d88d444
Simone <simone.wanderoy@gmail.com>
2018-11-09 12:56:55 +0100

    Add comment about $ and operator and list() function
    Add comment about $ and operator and list() function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix:
+## as matrix inversion usually is a costly computation,
+## caching of the inverse matrix might be a lot simpler 
+## and faster than repeated computing. 
 
-## Write a short comment describing this function
+## This function creates a "special" matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  Inv <- NULL
+  set <- function(y){
+    x <<- y
+    Inv <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) Inv <- inverse
+  getInverse <- function() Inv
+  list(set = set, 
+       get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the "special" matrix created by the previous function (makeCacheMatrix)
+## If the inverse has been calculated, the cacheSolve will 
+## retrieve the inverse from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  Inv <- x$getInverse()
+  if(!is.null(Inv)){
+    message("Cached data")
+    return(Inv)
+  }
+  mat <- x$get()
+  Inv <- inverse(mat, ...)
+  x$setInverse(Inv)
+  Inv
 }
